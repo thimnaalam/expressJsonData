393ee435e6bff67a36bf2fd2f1befa2a6478e63e
thimnaalam <alamthimna@gmail.com>
2024-02-13 08:38:56 +0200

    update

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello there");
+import express from 'express'
+
+// Express application
+const app = express()
+// Router
+const router = express.Router()
+// Port
+const port = +process.env.Port || 4000
+// Json url
+const dataURL ='file:///C:/Users/Students/Downloads/index.json'
+// Application middleware
+app.use(
+    router
+)
+// / => home
+router.get('^/$|/ejd', (req, res)=>{
+    res.json({
+        status: res.statusCode,
+        msg: 'Ola amigos'
+    })
+})
+// fetch all education
+router.get('/education',async (req, res)=>{
+  let {education} = (await fetch(dataURL)).json()
+  res.json({
+    status: res.statusCode,
+    education
+  })
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
